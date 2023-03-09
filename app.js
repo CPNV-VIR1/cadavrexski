eaa51be24bc87af36b55e3987594ddcc341376ad
lpodev <lpodevco@gmail.com>
2023-03-09 11:34:48 +0100

    Add translation EN,FR,DE

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const port = 8080;
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
-
+app.use("/assets",express.static(__dirname + "/assets"));
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
--- assets/js/cadavreXski.js
+++ assets/js/cadavreXski.js
@@ -2,9 +2,27 @@ document.addEventListener("DOMContentLoaded", function(){
     var inPhrase = document.getElementById("inPhrase");
     var btnSubmitPhrase = document.getElementById("btnSubmitPhrase");
     var btnRefresh = document.getElementById("btnRefresh")
+    var btnEn = document.getElementById("btnEn");
+    var btnFr = document.getElementById("btnFr");
+    var btnDe = document.getElementById("btnDe");
+
+    let language = navigator.language || navigator.userLanguage;
+    translate(language.substr(0, 2));
 
     inPhrase.focus();
     inPhrase.select();
+
+    btnEn.addEventListener("click", function(){
+        translate("en");
+    });
+
+    btnFr.addEventListener("click", function(){
+        translate("fr");
+    });
+
+    btnDe.addEventListener("click", function(){
+        translate("de");
+    });
 
     btnSubmitPhrase.addEventListener("click", function(){
         addPhrase(inPhrase.value);
@@ -25,3 +43,29 @@ function addPhrase(phrase){
 function refreshPhrases(){
     document.getElementById("divPhrases").textContent = "";
 }
+
+function translate(lang){
+    var langFile = "assets/lang/" + lang + ".json";
+    var defaultLangFile = "assets/lang/en.json";
+    var langData = null;
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", langFile, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200){
+            langData = JSON.parse(xhr.responseText);
+            document.getElementById("btnSubmitPhrase").textContent = langData.submit;
+            document.getElementById("btnRefresh").textContent = langData.refresh;
+            document.getElementById("lblPhrase").textContent = langData.labelPhrase;
+            document.getElementById("pAuthors").textContent = langData.title;
+            document.getElementById("inPhrase").placeholder = langData.input;
+            document.getElementById("lblInsertedPhrases").textContent = langData.labelInserted;
+            document.getElementById("pCopyright").textContent = langData.labelCopyright;
+    }else if (xhr.readyState == 4 && xhr.status == 404) {
+            xhr.open("GET", defaultLangFile, true);
+            xhr.send();
+        }
+    }
+    xhr.send();
+}
+
