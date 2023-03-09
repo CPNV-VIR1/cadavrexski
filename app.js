3676ef6d83584ef640e7c69aa65aedbfe4655a98
menoudyann <yann.menoud@cpnv.ch>
2023-03-09 10:34:24 +0100

    create server & app file

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const path = require('path');
+//app.js
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = 8080;
 
-// sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/index.html");
 });
-
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,7 @@
+//server.js
+const app = require("./app");
+const port = 8080;
+
+app.listen(port, () => {
+  console.log("Example app listening on port 8080!");
+});
