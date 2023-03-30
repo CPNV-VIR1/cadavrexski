851c526814ce3de3b76398315627f41e17e1b477
menoudyann <Yann.MENOUD@cpnv.ch>
2023-03-30 10:44:54 +0200

    create test folder

---
+++ test/phrase.test.js
@@ -0,0 +1,9 @@
+/**
+ * @file      phrase.test.js
+ * @brief     This class is designed to test the behaviour of the phrase controller.
+ * @author    Created by Menoud Yann
+ * @version   30-03-2023 - original (dedicated to RIA1)
+ * @version   30-03-2023 - update
+ */
+
+let PhraseController = require("../controllers/phrases.controller.js");
