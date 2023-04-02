65e55e41ef94442ad64915b3fbec8ff09daf760d
menoudyann <Yann.MENOUD@cpnv.ch>
2023-04-02 09:44:07 +0200

    add tests to test phrase controller

--- test/phrase.controller.test.js
+++ test/phrase.controller.test.js
@@ -6,10 +6,24 @@
  * @version   30-03-2023 - update
  */
 
-const request = require('supertest')
-const { app } = require('../app')
+const request = require("supertest");
+const { app } = require("../app");
 
 test("test the root path", async () => {
-    const response = await request(app).get("/api/v1/phrases")
-    expect(response.statusCode).toBe(200)
-  });
+  const response = await request(app).get("/api/v1/phrases");
+  expect(response.statusCode).toBe(200);
+});
+
+test("test the store phrase method", async () => {
+  myTestPhrase = 'My test phrase';
+  const response = await request(app)
+    .post("/api/v1/phrases")
+    .send({phrase: 'My test phrase'});
+    expect(response.body).toHaveProperty("phrase", myTestPhrase);
+});
+
+test("test the destroy all phrases method", async () => { 
+  const response = await request(app)
+  .delete("/api/v1/phrases/destroy");
+  expect(response.body).toHaveProperty("phrase", "All phrases deleted");
+});
